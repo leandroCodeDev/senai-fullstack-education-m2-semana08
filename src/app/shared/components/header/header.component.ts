a3d2f360bff826f1f4f82d78832f5ad52946b5bc
Leandro Dias <lvd@certi.org.br>
2024-07-07 17:23:44 -0300

    exercicio 1 e 2 concluidos

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+import { CommonModule } from '@angular/common';
 
+interface User{
+  nome:String,
+  admin:boolean
+}
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent,CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  @Input() user:User = {nome:"lorem",admin:false}
+  showSideBar = false
 }
--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { Component,Input, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [RouterModule,CommonModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
+  @Input() admin=false
+  
+  constructor(){}
+
+  ngOnInit(){
+
+  }
+
 
 }
