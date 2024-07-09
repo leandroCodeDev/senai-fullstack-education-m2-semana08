57315f18257fb3fab62da749e0f8e4930faaf5d8
Leandro Dias <lvd@certi.org.br>
2024-07-08 22:25:31 -0300

    aplicado lista de curso no form

--- src/app/cadastro-aluno/cadastro-aluno.component.ts
+++ src/app/cadastro-aluno/cadastro-aluno.component.ts
@@ -10,6 +10,8 @@ import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { HeaderComponent } from '../shared/components/header/header.component';
 import { UsuariosService } from '../shared/services/usuarios/usuarios.service';
 import { UsuarioInterface } from '../shared/interfaces/usuario.interface';
+import { CursosService } from '../shared/services/cursos/cursos.service';
+import { CursoInterface } from '../shared/interfaces/curso.interface';
 
 @Component({
   selector: 'app-cadastro-aluno',
@@ -21,16 +23,18 @@ import { UsuarioInterface } from '../shared/interfaces/usuario.interface';
 export class CadastroAlunoComponent implements OnInit {
   formCadastro!: FormGroup;
   idUsuario: string | undefined;
+  cursos!:Array<CursoInterface>;
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private usuarioService: UsuariosService,
+    private cursoService: CursosService,
     private router: Router,
   ){}
   ngOnInit(): void {
     this.idUsuario = this.activatedRoute.snapshot.params['id'];
 
-    console.log(this.idUsuario)
-
+    
     this.formCadastro = new FormGroup({
       nome: new FormControl('', Validators.required),
       cpf: new FormControl('', Validators.required),
@@ -47,6 +51,11 @@ export class CadastroAlunoComponent implements OnInit {
         }
       });
     }
+    this.cursoService.getCursos().subscribe((retorno) => {
+      console.log(retorno)
+        this.cursos = retorno;
+    });
+
   }
 
 
